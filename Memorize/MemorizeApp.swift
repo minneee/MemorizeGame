79d69f7d6bb8e72fb41d931704c0f00e9d895059
minneee <alsgml022100@naver.com>
2023-10-04 03:19:07 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 김민희 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
